2f985022d9215360e45bf6e935b7f5d13ebdc271
Francisco Marcolino <fmarcolino@live.com>
2021-04-13 22:52:36 -0300

    hello: add example 2

--- examples/hello.py
+++ examples/hello.py
@@ -6,3 +6,15 @@ def hello_world():
     d = jp.Div(text='Hello world!')
     wp.add(d)
     return wp
+
+
+def hello_world_02():
+    wp = jp.WebPage()
+    my_paragraph_design = (
+        "w-64 bg-blue-500 m-2 hover:bg-blue-700 "
+        "text-white font-bold py-2 px-4 rounded"
+    )
+
+    for i in range(1,11):
+        jp.P(text=f'{i}) Hello World!', a=wp, classes=my_paragraph_design)
+    return wp
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from examples import (
 
 pages = [
     hello.hello_world,
+    hello.hello_world_02,
 ]
 
 
